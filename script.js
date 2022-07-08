62880ca15698e52cf03cdb3b3de32afdf26b03b0
Jalapo <javenporter@gmail.com>
2022-07-07 22:32:19 -0500

    Added layout and slide-in animations for projects section

---
+++ script.js
@@ -0,0 +1,16 @@
+let pos = 0; // hidden
+
+function moveProjects() {
+    let projects = document.querySelector(".projects");
+    if (pos == 0) { // if projects are hidden
+        projects.classList.add('unhidden');
+        projects.classList.remove('hidden');
+        setTimeout(projects.scrollIntoView({behavior:"smooth"}, 2001));
+        pos = 1;
+    } else if (pos == 1) {
+        projects.classList.add('hidden');
+        projects.classList.remove('unhidden');
+        pos = 0;
+    }
+    
+}
